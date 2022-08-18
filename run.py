90bd4c14e99a5d4f1d122db4f8da2bb6dd8b7fc3
navdeepkaushish <navdeepkaushish@gmail.com>
2022-08-18 09:33:35 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -1,4 +1,184 @@
 # -*- coding: utf-8 -*-
+<<<<<<< HEAD
+"""
+Created on Wed Aug 17 10:52:45 2022
+
+@author: Navdeep Kumar
+"""
+
+from __future__ import print_function
+
+import glob
+import os
+import sys
+import json
+from shapely.geometry import Point, Polygon
+
+from PIL import Image
+import cv2 as cv
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
+import tensorflow.keras.backend as K
+from utils import *
+from loss_functions import *
+
+import argparse
+import json
+import logging
+
+from cytomine import Cytomine
+from cytomine import CytomineJob
+from cytomine.models import (
+	Property,
+	Annotation,
+	AnnotationTerm,
+	AnnotationCollection,
+	Project,
+	ImageInstanceCollection,
+	Job)
+#==============================================================================
+def main(argv):
+	with CytomineJob.from_cli(argv) as conn:
+		conn.job.update(status=Job.RUNNING, progress=0, statusComment='Intialization...')
+		base_path = "{}".format(os.getenv('HOME'))  # Mandatory for Singularity
+		working_path = os.path.join(base_path, str(conn.job.id))
+
+
+		# Select images to process
+		images = ImageInstanceCollection().fetch_with_filter('project', conn.parameters.cytomine_id_project)
+		if conn.parameters.cytomine_id_images != 'all':  # select only given image instances = [image for image in image_instances if image.id in id_list]
+			images = [_ for _ in images if _.id
+					  in map(lambda x: int(x.strip()),
+							 conn.parameters.cytomine_id_images.split(','))]
+		#images_id = [image.id for image in images]
+
+		# Download selected images into 'working_directory'
+		img_path = os.path.join(working_path, 'images')
+		# if not os.path.exists(img_path):
+		os.makedirs(img_path)
+
+		for image in conn.monitor(
+				images, start=2, end=50, period=0.1,
+				prefix='Downloading images into working directory...'):
+			fname, fext = os.path.splitext(image.filename)
+			if image.download(dest_pattern=os.path.join(
+					img_path,
+					"{}{}".format(image.id, fext))) is not True:  # images are downloaded with image_ids as names
+				print('Failed to download image {}'.format(image.filename))
+
+		#========= loading model according to fish type =======================
+
+		if conn.parameters.fish =='zebrafish':
+			with tf.device('/cpu:0'):
+				model = load_model('/models/zebra_hrnet.hdf5', compile=True)
+		elif conn.parameters.fish == 'medaka':
+			with tf.device('/cpu:0'):
+				model = load_model('/models/medaka_hrnet.hdf5', compile=True)
+		else:
+			with tf.device('/cpu:0'):
+				model = load_model('/models/seabream_hrnet.hdf5', compile=True)
+
+		 # Prepare image file paths from image directory for execution
+		conn.job.update(progress=50,
+						statusComment="Preparing data for execution..")
+		image_paths = glob.glob(os.path.join(img_path, '*'))
+
+		model_size = conn.paremeters.model_size #256x256 in our case
+		N = conn.parameters.n_landmarks
+
+		for i in range(len(img_path)):
+
+			org_img = cv.imread(img_path[i], cv.IMREAD_UNCHANGED)
+			filename = os.path.basename(image_paths[i])[:-4]
+			#fname = int(fname)
+
+			re_img, re_lm = rescale_pad_img(org_img, None, model_size)
+
+			tf_img = array_to_img(re_img)
+			tf_img = tf.cast(tf_img, tf.float32) / 255.0  # normalize the image
+			tf_img = tf.expand_dims(tf_img, 0)
+
+			pred_mask = model.predict(tf_img)
+			pred_mask = np.reshape(pred_mask, newshape=(model_size,model_size, N))
+
+			lm_list = []
+			for j in range(N):
+				x,y =  maskToKeypoints(pred_mask[:, :, j])
+				lm_list.append((x,y))
+
+			pred_lm = np.array(lm_list)
+			up_size = org_img.shape[:2]
+			lm = up_lm(pred_lm, model_size, up_size)
+			image_id = next((x.id for x in images if x.originalFilename == filename), None)
+
+			lm1 = Point(lm[0])
+			lm2 = Point(lm[1])
+			lm3 = Point(lm[2])
+			lm4 = Point(lm[3])
+			lm5 = Point(lm[4])
+			lm6 = Point(lm[5])
+			lm7 = Point(lm[6])
+			lm8 = Point(lm[7])
+			lm9 = Point(lm[8])
+			lm10 = Point(lm[9])
+			lm11 = Point(lm[10])
+			lm12 = Point(lm[11])
+			lm13 = Point(lm[12])
+			lm14 = Point(lm[13])
+			lm15 = Point(lm[14])
+			lm16 = Point(lm[15])
+			lm17 = Point(lm[16])
+			lm18 = Point(lm[17])
+			lm19 = Point(lm[18])
+			lm20 = Point(lm[19])
+			lm21 = Point(lm[20])
+			lm22 = Point(lm[21])
+			lm23 = Point(lm[22])
+			lm24 = Point(lm[23])
+			lm25 = Point(lm[24])
+
+			annotations = AnnotationCollection()
+			annotations.append(Annotation(location=lm1.wkt, id_image=image_id, id_terms = 549311142, id_project=549311125))
+			annotations.append(Annotation(location=lm2.wkt, id_image=image_id, id_terms = 549311148, id_project=549311125))
+			annotations.append(Annotation(location=lm3.wkt, id_image=image_id, id_terms = 549311156, id_project=549311125))
+			annotations.append(Annotation(location=lm4.wkt, id_image=image_id, id_terms = 549311162, id_project=549311125))
+			annotations.append(Annotation(location=lm5.wkt, id_image=image_id, id_terms = 549311170, id_project=549311125))
+			annotations.append(Annotation(location=lm6.wkt, id_image=image_id, id_terms=549311176, id_project=549311125))
+			annotations.append(Annotation(location=lm7.wkt, id_image=image_id, id_terms=549311184, id_project=549311125))
+			annotations.append(Annotation(location=lm8.wkt, id_image=image_id, id_terms=549311190, id_project=549311125))
+			annotations.append(Annotation(location=lm9.wkt, id_image=image_id, id_terms=549311198, id_project=549311125))
+			annotations.append(Annotation(location=lm10.wkt, id_image=image_id, id_terms=549311204, id_project=549311125))
+			annotations.append(Annotation(location=lm11.wkt, id_image=image_id, id_terms=549311212, id_project=549311125))
+			annotations.append(Annotation(location=lm12.wkt, id_image=image_id, id_terms=549311218, id_project=549311125))
+			annotations.append(Annotation(location=lm13.wkt, id_image=image_id, id_terms=549311226, id_project=549311125))
+			annotations.append(Annotation(location=lm14.wkt, id_image=image_id, id_terms=549311232, id_project=549311125))
+			annotations.append(Annotation(location=lm15.wkt, id_image=image_id, id_terms=549311240, id_project=549311125))
+			annotations.append(Annotation(location=lm16.wkt, id_image=image_id, id_terms=549311246, id_project=549311125))
+			annotations.append(Annotation(location=lm17.wkt, id_image=image_id, id_terms=549311254, id_project=549311125))
+			annotations.append(Annotation(location=lm18.wkt, id_image=image_id, id_terms=549311262, id_project=549311125))
+			annotations.append(Annotation(location=lm19.wkt, id_image=image_id, id_terms=549311270, id_project=549311125))
+			annotations.append(Annotation(location=lm20.wkt, id_image=image_id, id_terms=549311276, id_project=549311125))
+			annotations.append(Annotation(location=lm21.wkt, id_image=image_id, id_terms=549311282, id_project=549311125))
+			annotations.append(Annotation(location=lm22.wkt, id_image=image_id, id_terms=549311290, id_project=549311125))
+			annotations.append(Annotation(location=lm23.wkt, id_image=image_id, id_terms=549311296, id_project=549311125))
+			annotations.append(Annotation(location=lm24.wkt, id_image=image_id, id_terms=549311304, id_project=549311125))
+			annotations.append(Annotation(location=lm25.wkt, id_image=image_id, id_terms=549311310, id_project=549311125))
+			annotations.save()
+
+		conn.job.update(status=Job.TERMINATED, status_comment="Finish", progress=100)  # 524787186
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
+
+
+
+
+
+=======
 
 #
 # * Copyright (c) 2009-2016. Authors: see NOTICE file.
@@ -215,3 +395,4 @@ def main(argv):
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
+>>>>>>> 24bfdbb97aee6fa1bfe33df0847843090d1e351c
