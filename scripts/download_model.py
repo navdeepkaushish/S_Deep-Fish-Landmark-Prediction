1e2d821d80efb43556b88b85dc4100c0eab93f38
navdeepkaushish <navdeepkaushish@gmail.com>
2022-10-13 16:19:01 +0200

    Update download_model.py

--- scripts/download_model.py
+++ scripts/download_model.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     dir_path = 'models/'
-    r = requests.get('https://sandbox.zenodo.org/api/deposit/depositions', params={'access_token': sys.argv[1]})
+    r = requests.get('https://zenodo.org/api/deposit/depositions', params={'access_token': sys.argv[1]})
     for file in r.json()[0]['files']:
         print(file['filename'])
         response = requests.get(file['links']['download'])
